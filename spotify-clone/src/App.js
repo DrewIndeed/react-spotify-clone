c5fe0c99a033ab793553fafeb645b8a186064a33
Andrew Le <s3820098@rmit.edu.vn>
2021-09-03 21:34:33 +0700

    use get token fucntion to verify log in

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
+import { getTokenFromUrl } from "./Components/Spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) setToken(_token);
+  }, [token]);
+
+  console.log(token);
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
