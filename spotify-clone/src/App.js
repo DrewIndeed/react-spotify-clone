e558ead0738712039b16075f9bf67adbea0f93c2
Andrew Le <s3820098@rmit.edu.vn>
2021-09-03 21:56:39 +0700

    get user info

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
 import { getTokenFromUrl } from "./Components/Spotify";
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -10,7 +13,15 @@ function App() {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    if (_token) setToken(_token);
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      // get the user account
+      spotify.getMe().then(user => {
+        // show me the user
+        console.log(user);
+      });
+    }
   }, [token]);
 
   console.log(token);
