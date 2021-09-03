3a10908ce9966642fb45649a66948975dd308e60
Andrew Le <s3820098@rmit.edu.vn>
2021-09-03 20:46:15 +0700

    create the spotify bridge to authenticate users

---
+++ spotify-clone/src/Components/Spotify.js
@@ -0,0 +1,21 @@
+export const authEndPoint = 'https://accounts.spotify.com/authorize';
+
+// client id:  0691dd4aa01640f6a0cae32ed821e4ce
+const redirectUri = 'http://localhost:3000/';
+const clientId = ' 0691dd4aa01640f6a0cae32ed821e4ce';
+
+const scope = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndPoint}?cilent_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&response_type=token&show_dialog=true`;
+
+
+
+
+
+
