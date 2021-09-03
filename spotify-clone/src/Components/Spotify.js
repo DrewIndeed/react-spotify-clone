145aa6894f2fad7ef2db985037d756b907c8162f
Andrew Le <s3820098@rmit.edu.vn>
2021-09-03 20:56:09 +0700

    fix typo and consistency

--- spotify-clone/src/Components/Spotify.js
+++ spotify-clone/src/Components/Spotify.js
@@ -1,8 +1,8 @@
 export const authEndPoint = 'https://accounts.spotify.com/authorize';
 
 // client id:  0691dd4aa01640f6a0cae32ed821e4ce
-const redirectUri = 'http://localhost:3000/';
-const clientId = ' 0691dd4aa01640f6a0cae32ed821e4ce';
+const redirectUri = 'http://localhost:3000';
+const clientId = '0691dd4aa01640f6a0cae32ed821e4ce';
 
 const scope = [
     "user-read-currently-playing",
@@ -12,7 +12,9 @@ const scope = [
     "user-modify-playback-state",
 ];
 
-export const loginUrl = `${authEndPoint}?cilent_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&response_type=token&show_dialog=true`;
+export default function loginUrl() {
+    return `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&response_type=token&show_dialog=true`;
+}
 
 
 
