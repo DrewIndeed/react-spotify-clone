598909bf2310bf5817c48f67964258623a1cc1e4
Andrew Le <s3820098@rmit.edu.vn>
2021-09-09 15:36:10 +0700

    Add body image and info and styling

--- spotify-clone/src/Components/Body.js
+++ spotify-clone/src/Components/Body.js
@@ -6,6 +6,14 @@ function Body( props ) {
     return (
         <div className="body">
             <Header spotify={props.spotify}></Header>
+            <div className="body__info">
+                <img src="https://assets.website-files.com/5c1426cc1ac48f15ca9c8e22/5ee167c942a25f251fb308fa_discover%20weekly.jpg" alt="discovery thumbnail" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>Description...</p>
+                </div>
+            </div>
         </div>
     )
 }
