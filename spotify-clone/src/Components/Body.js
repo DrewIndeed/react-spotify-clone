4ad8ee8efece06c6134296c740f16953f06f6994
Andrew Le <s3820098@rmit.edu.vn>
2021-09-09 16:41:04 +0700

    list all album songs dynamically and add scrolling

--- spotify-clone/src/Components/Body.js
+++ spotify-clone/src/Components/Body.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import './Body.css';
-import Header from './Header';
+import React from "react";
+import "./Body.css";
+import Header from "./Header";
 import { useDataLayerValue } from "./DataLayer";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 function Body(props) {
-    const [{ norah_jones }] = useDataLayerValue();
-    return (
-        <div className="body">
-            <Header spotify={props.spotify}></Header>
-            <div className="body__info">
-                <img src={ norah_jones?.images[0].url } alt="discovery thumbnail" />
-                <div className="body__infoText">
-                    <strong>PLAYLIST</strong>
-                    <h2>{ norah_jones?.name }</h2>
-                    <p>{ norah_jones?.description }</p>
-                </div>
-            </div>
-
-            <div className="body__songs">
-                <div className="body__icons">
-                    <PlayCircleFilledIcon className="body__shuffle"></PlayCircleFilledIcon>
-                    <FavoriteIcon fontSize="large"></FavoriteIcon>
-                    <MoreHorizIcon></MoreHorizIcon>
-                </div>
-                <div className="songs_list">
-
-                </div>
-            </div>
+  const [{ norah_jones }] = useDataLayerValue();
+  return (
+    <div className="body">
+      <Header spotify={props.spotify}></Header>
+      <div className="body__info">
+        <img src={norah_jones?.images[0].url} alt="discovery thumbnail" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>{norah_jones?.name}</h2>
+          <p>{norah_jones?.description}</p>
         </div>
-    )
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle"></PlayCircleFilledIcon>
+          <FavoriteIcon fontSize="large"></FavoriteIcon>
+          <MoreHorizIcon></MoreHorizIcon>
+        </div>
+        {norah_jones?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Body
+export default Body;
